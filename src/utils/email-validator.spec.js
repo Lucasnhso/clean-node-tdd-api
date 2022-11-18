d06673abd346cf7f8222cc1286b17fe1a59e566e
Lucas <lucashenrique.d.s.o@gmail.com>
2022-11-18 16:15:03 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -9,15 +9,21 @@ const makeSut = () => {
   return new EmailValidator()
 }
 describe('Email Validator', () => {
-  test('should return true if validator return true', () => {
+  test('Should return true if validator return true', () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
-  test('should return false if validator return false', () => {
+  test('Should return false if validator return false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+  test('Should call validator with correct email', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
