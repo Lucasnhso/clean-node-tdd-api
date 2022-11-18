e45cb3bc654508c960d5455a4de5eb500ef0f50c
Lucas <lucashenrique.d.s.o@gmail.com>
2022-11-18 16:11:12 -0300

    move sut create to a factory helper method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -5,16 +5,18 @@ class EmailValidator {
     return validator.isEmail(email)
   }
 }
-
+const makeSut = () => {
+  return new EmailValidator()
+}
 describe('Email Validator', () => {
   test('should return true if validator return true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
   test('should return false if validator return false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
